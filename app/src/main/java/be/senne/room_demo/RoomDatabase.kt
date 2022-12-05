a5e56a6ad6b5d373307eaa3f4513806f1885f9a4
Senne <senne.de.neef@student.ehb.be>
2022-12-05 16:44:05 +0100

    fix, database moest inheriten

--- app/src/main/java/be/senne/room_demo/RoomDatabase.kt
+++ app/src/main/java/be/senne/room_demo/RoomDatabase.kt
@@ -1,8 +1,9 @@
 package be.senne.room_demo
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
-@Database(entities = [Entity::class], version = 1)
-abstract class RoomDatabase {
+@Database(entities = [Entity::class], version = 1, exportSchema = false)
+abstract class RoomDatabase : RoomDatabase() {
     abstract fun entityDAO() : EntityDAO
 }
