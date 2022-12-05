b9c789523382c8d14c868b0a01d6bf2bd060d434
Senne <senne.de.neef@student.ehb.be>
2022-12-05 19:29:46 +0100

    toon db items in een lijst

--- app/src/main/java/be/senne/room_demo/MainActivity.kt
+++ app/src/main/java/be/senne/room_demo/MainActivity.kt
@@ -1,9 +1,11 @@
 package be.senne.room_demo
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -18,17 +20,33 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Button
+import androidx.compose.ui.unit.dp
+
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        lateinit var context: Context
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        context = applicationContext
 
         val entity = Entity(null, "Een interessante titel", "Een goede beschrijving")
 
         val db = Room.databaseBuilder(applicationContext, RoomDatabase::class.java, "mijn-database").build()
         val entityDAO = db.entityDAO()
 
+        var entities : List<Entity> = listOf()
+
         CoroutineScope(Dispatchers.Main).launch {
-            val entities = entityDAO.getAll()
+            entities = entityDAO.getAll()
             if(entities.isEmpty()) {
                 entityDAO.insert(entity)
                 Toast.makeText(applicationContext, "Entity is aan database toegevoegd.", Toast.LENGTH_SHORT).show()
@@ -36,17 +54,44 @@ class MainActivity : ComponentActivity() {
             else {
                 Toast.makeText(applicationContext, "Entity is al in database (${entities[0].description})", Toast.LENGTH_SHORT).show()
             }
-        }
-
-        setContent {
-            Room_demoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            setContent {
+                Room_demoTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                        drawDb(entities)
+                    }
                 }
-            }
         }
     }
+}
+
+@Composable
+fun drawDb(entities : List<Entity>) {
+
+    val state = rememberLazyListState()
+
+    LazyColumn(
+        contentPadding = PaddingValues(start = 24.dp, end = 24.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        state = state
+    ) {
+        item {
+            Button(onClick = {
+                CoroutineScope(Dispatchers.Main).launch {
+                    val entity = Entity(null, "Entity0", "Desc")
+                    RoomDatabase.instance.value.entityDAO().insert(entity)
+                }
+            }) {
+                Text("Add Entity")
+            }
+        }
+
+        itemsIndexed(entities) { index, entity ->
+            Text("$index: ${entity.title}")
+        }
+
+    }
+}
 }
 
 @Composable
--- app/src/main/java/be/senne/room_demo/RoomDatabase.kt
+++ app/src/main/java/be/senne/room_demo/RoomDatabase.kt
@@ -1,9 +1,17 @@
 package be.senne.room_demo
 
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Entity::class], version = 1, exportSchema = false)
 abstract class RoomDatabase : RoomDatabase() {
     abstract fun entityDAO() : EntityDAO
+
+    companion object {
+        var instance = lazy {
+            Room.databaseBuilder(MainActivity.context, be.senne.room_demo.RoomDatabase::class.java, "mijn-database").build()
+        }
+    }
+
 }
